9d1ca76317c24a05e82009d0b35721fbdc68a31f
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2022-12-23 11:57:17 +0900

    it's gonna fail lint

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+)
+
+func split(sum int) (x, y int, z float32) {
+	x = sum * 4 / 9
+	x *= 2
+	y = sum - x
+	y += 3
+	x += 5
+	z = 1.
+
+	//dasfsdafasdffsdasdafsdfsdaffsdaasdfafsdsdfa
+	//dd
+
+	//asdfsdaf
+
+	return
+}
+
 func main() {
-	println("Hello world")
+	fmt.Println(split(17))
 }
